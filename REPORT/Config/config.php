d7f74523ea66a0c2f4a8ca0c42b37de2e9b05385
Sandra <153555175+Raddsan@users.noreply.github.com>
2024-09-10 15:35:57 +0200

    Add files via upload

---
+++ REPORT/Config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Report all type of errors
+error_reporting(-1);
+
+// Display all errors
+ini_set('display_errors', '1');
+
+<?php
+// Set the timezone to use
+date_default_timezone_set('Europe/Stockholm');
+
+// The date of today
+$today = date('Y-m-d H:i:s');
+
+// Name of the week day
+$weekday = date('l');
